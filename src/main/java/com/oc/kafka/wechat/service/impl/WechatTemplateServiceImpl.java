fee2da35e27c7f7eb5c18b9e0161c91227918f06
pingzhu <13774495234@163.com>
2023-10-09 21:02:49 +0800

    A

--- src/main/java/com/oc/kafka/wechat/service/impl/WechatTemplateServiceImpl.java
+++ src/main/java/com/oc/kafka/wechat/service/impl/WechatTemplateServiceImpl.java
@@ -43,6 +43,7 @@ public class WechatTemplateServiceImpl implements WechatTemplateService {
 //        if (webchatTemplateProperties.getTemplateResultType() == 0) {
 //            return FileUtils.
 //        }
+        
         return null;
     }
 }
